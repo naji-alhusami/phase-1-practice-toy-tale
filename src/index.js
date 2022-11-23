c8f4fcb2f180441c14f21aa4ccf3deca380ded61
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2022-11-23 14:28:58 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -3,13 +3,71 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const formSubmit = document.querySelector(".add-toy-form");
+
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
     } else {
       toyFormContainer.style.display = "none";
     }
+  });
+
+  const divToys = document.getElementById("toy-collection");
+  fetch("http://localhost:3000/toys")
+    .then((response) => response.json())
+    .then((data) => {
+      data.map((toy) => {
+        const divToy = document.createElement("div");
+        divToy.setAttribute("class", "card");
+        divToy.innerHTML = `
+        <h2>${toy.name}</h2>
+        <img src=${toy.image} class="toy-avatar" />
+        <p>${toy.likes} Likes </p>
+        <button class="like-btn" id=${toy.id} likes=${toy.likes} onclick="">Likes</button>
+        `;
+        divToys.appendChild(divToy);
+      });
+
+      const likeButtons = document.querySelectorAll(".like-btn");
+      for (let i = 0; i < likeButtons.length; i++) {
+        likeButtons[i].addEventListener("click", (e) => {
+          let counter = data[i].likes;
+          e.preventDefault();
+          fetch(`http://localhost:3000/toys/${e.target.id}`, {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+            body: JSON.stringify({
+              likes: counter + 1,
+            }),
+          });
+        });
+      }
+    });
+
+  const nameInput = document.getElementsByName("name")[0];
+  const imgInput = document.getElementsByName("image")[0];
+
+  console.log(imgInput);
+
+  formSubmit.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: nameInput.value,
+        image: imgInput.value,
+        likes: 0,
+      }),
+    });
   });
 });
